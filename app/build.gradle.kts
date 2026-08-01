715143ebb69cbc5223878826cbced536eaf5e7c3
alinmarian2024-a11y <alinmarian2024@gmail.com>
2026-08-01 17:21:57 +0300

    Set final Carpathia Games application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.romania2048.app"
+    applicationId = "com.carpathiagames.romania2048"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
