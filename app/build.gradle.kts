2cbeaf131044840b70be90fa53b66997ebd979f1
alinmarian2024-a11y <alinmarian2024@gmail.com>
2026-08-01 13:36:13 +0300

    Remove build type configurations from build.gradle.kts
    Removed release and debug build type configurations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,15 +55,6 @@ buildTypes {
     }
 }
 
-  buildTypes {
-    release {
-      isCrunchPngs = false
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
-    }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
-  }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
